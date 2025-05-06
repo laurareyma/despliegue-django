eee3c3632f319d9a0c14ccfcd9c9a9f4c6fc579b
Laura Vanesa Reyes <147190386+laurareyma@users.noreply.github.com>
2025-05-06 17:58:08 -0500

    aplicando cambios

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate 
+
+# Apply database migrations
+python manage.py migrate
 
 git update-index --chmod=+x build.sh 
