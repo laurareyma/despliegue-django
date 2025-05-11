adcdc0363ddbf38eb1580dc7407680f1df44e301
Laura Vanesa Reyes <147190386+laurareyma@users.noreply.github.com>
2025-05-11 11:17:27 -0500

    Actualización de la vista en render

--- despliegue/settings.py
+++ despliegue/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ohxhze*x@a0k%xarp3_d$hh-@y6a^ri=90#c*$6wal-hqcx5sk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 localhost").split(' ') if not DEBUG else []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 localhost .onrender.com").split(' ')
 
 
 # Application definition
