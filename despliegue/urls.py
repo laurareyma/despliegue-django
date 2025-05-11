1e11a812cf63ab4f26341a7c6e98328735d60681
Laura Vanesa Reyes <147190386+laurareyma@users.noreply.github.com>
2025-05-11 10:32:36 -0500

    actualización de autores

--- despliegue/urls.py
+++ despliegue/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from doc import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authors/', views.authors, name='authors'),
 ]
--- doc/views.py
+++ doc/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def authors(request):
+    authors = [
+        'Laura Vanesa Reyes',
+        'Ana María Cordero',
+    ]
+    return render(request, 'doc/authors.html', {'authors': authors})
